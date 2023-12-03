1f579fc5a2cfa0ca0b67dbe8dfd1b92e6d5c3f40
Jason D'Oyley <jdfd.dev@icloud.com>
2023-12-02 19:47:09 -0500

    Add check require f(x) and refactor code

--- js/script.js
+++ js/script.js
@@ -27,33 +27,52 @@ function isValidEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+// Check required fields
+function checkRequired(inputs) {
+  inputs.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is requred.`);
+    } else {
+      showSuccess(input);
+    }
+    console.log(input.value);
+  });
+}
+
+// Get field name function
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event Listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  if (username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username);
-  }
+  // if (username.value === '') {
+  //   showError(username, 'Username is required');
+  // } else {
+  //   showSuccess(username);
+  // }
 
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  } else if (!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(email);
-  }
+  // if (email.value === '') {
+  //   showError(email, 'Email is required');
+  // } else if (!isValidEmail(email.value)) {
+  //   showError(email, 'Email is not valid');
+  // } else {
+  //   showSuccess(email);
+  // }
 
-  if (password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password);
-  }
+  // if (password.value === '') {
+  //   showError(password, 'Password is required');
+  // } else {
+  //   showSuccess(password);
+  // }
 
-  if (password2.value === '') {
-    showError(password2, 'Password 2 is required');
-  } else {
-    showSuccess(password2);
-  }
+  // if (password2.value === '') {
+  //   showError(password2, 'Password 2 is required');
+  // } else {
+  //   showSuccess(password2);
+  // }
+
+  checkRequired([username, email, password, password2]);
 });
